b5bfd806ac1a52979c527ba73c5838d4f2aece93
brianmsj <brianmsj@gmail.com>
2017-03-08 12:00:21 -0800

    made state in list

--- js/components/list.js
+++ js/components/list.js
@@ -4,16 +4,28 @@ import Card from './card';
 
 
 export default class List extends React.Component {
-  render() {
-    
+  constructor (props) {
+    super(props);
+    this.state = {
+      render: false,
+    };
+    this.renderCard = this.renderCard.bind(this);
+  }
+
+    renderCard(){
+      this.setState({
+        render: !this.state.render
+      });
+    }
+
+
+    render(){
+
     return (
       <div className = "list"> GUEST FILTERS
+       <button type="submit" onClick = {this.renderCard}> </button>
      	 <Card/>
-
     </div>
     );
   }
 }
-
-
-
